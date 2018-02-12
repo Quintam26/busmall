e14bfefb825dcc38cefe5ce5e700911c071b1c70
Mario Quintana <pyrros2000@gmail.com>
2018-02-12 14:22:41 -0800

    adding image files and constructor function

---
+++ app.js
@@ -0,0 +1,15 @@
+function Product() {
+
+    this.count = 0;
+    this.iamge = [];
+
+
+}
+
+
+
+
+
+const image1 = document.getElementById('#image1');
+const image2 = document.getElementById('#image2');
+const image3 = document.getElementById('#image3');
