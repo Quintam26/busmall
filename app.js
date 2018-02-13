58cc10770f138aa66985f1ed0bcf9efdac5d544c
Mario Quintana <pyrros2000@gmail.com>
2018-02-12 16:33:44 -0800

    reorganized

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
-function Product(imagePath, imageName) {
+const game = {
+    product = [],
+    start: function (){
+
+        this.product.push(
+            new Product('img/banana.jpg', 'banana'),
+            new Product('img/bathroom.jpg', 'bathroom'),
+            new Product('img/boots.jpg', 'boots'),
+            new Product('img/breakfast.jpg', 'breakfast'),
+            new Product('img/bubblegum.jpg', 'bubblegum'),
+            new Product('img/chair.jpg', 'chair'),
+            new Product('img/cthulhu.jpg', 'cthulu'),
+            new Product('img/dog-duck.jpg', 'dog-duck'),
+            new Product('img/dragon.jpg', 'dragon'),
+            new Product('img/pen.jpg', 'pen'),
+            new Product('img/pet-sweep.jpg', 'pet-sweep'),
+            new Product('img/scissors.jpg', 'scissors'),
+            new Product('img/shark.jpg', 'shark'),
+            new Product('img/sweep.jpg', 'sweep'),
+            new Product('img/tauntaun.jpg', 'tauntaun'),
+            new Product('img/unicorn.jpg', 'unicorn'),
+            new Product('img/usb.jpg', 'usb'),
+            new Product('img/water-can.jpg', 'water-can'),
+            new Product('img/wine-glass.jpg', 'wine-glass'),
+        );
+
+        this.showProduct();
+    },
+    showProduct: function() {
+
+        }
+};
+
+game.start();
+
+function Product (imagePath, imageName) {
 
     this.count = 0;
     this.imagePath = imagePath;
     this.imageName = imageName;
     
 }
-
-const image1 = document.getElementById('#image1');
-const image2 = document.getElementById('#image2');
-const image3 = document.getElementById('#image3');
-
-const images = [];
-images.push(new Product(imagePath, imageName));
-images.push(new Product('img/banana.jpg', 'banana'));
-images.push(new Product('img/bathroom.jpg', 'bathroom'));
-images.push(new Product('img/boots.jpg', 'boots'));
-images.push(new Product('img/breakfast.jpg', 'breakfast'));
-images.push(new Product('img/bubblegum.jpg', 'bubblegum'));
-images.push(new Product('img/chair.jpg', 'chair'));
-images.push(new Product('img/cthulhu.jpg', 'cthulu'));
-images.push(new Product('img/dog-duck.jpg', 'dog-duck'));
-images.push(new Product('img/dragon.jpg', 'dragon'));
-images.push(new Product('img/pen.jpg', 'pen'));
-images.push(new Product('img/pet-sweep.jpg', 'pet-sweep'));
-images.push(new Product('img/scissors.jpg', 'scissors'));
-images.push(new Product('img/shark.jpg', 'shark'));
-images.push(new Product('img/sweep.jpg', 'sweep'));
-images.push(new Product('img/tauntaun.jpg', 'tauntaun'));
-images.push(new Product('img/unicorn.jpg', 'unicorn'));
-images.push(new Product('img/usb.jpg', 'usb'));
-images.push(new Product('img/water-can.jpg', 'water-can'));
-images.push(new Product('img/wine-glass.jpg', 'wine-glass'));
-
+/*
 function random(){
     return Math.floor(Math.random() * images.length); 
 };
 
+
 function display (){
-    
+    let i = 0;
     image1 = random();
 
     image2 = random();
@@ -49,6 +59,9 @@ function display (){
     while(image3===image2||image1) {
         random();
     }
-
 };
-display();
+
+const image1 = document.getElementById('#image1');
+const image2 = document.getElementById('#image2');
+const image3 = document.getElementById('#image3');
+*/
