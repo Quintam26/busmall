45f79a6a2cefe801c4422b4864bb4146ce506d70
Mario Quintana <pyrros2000@gmail.com>
2018-02-16 08:01:45 -0800

    started settings form  in html and js

---
+++ JS/settings.js
@@ -0,0 +1,10 @@
+const settingsForm = document.getElementById('settings-form');
+settingsForm.addEventListener('submit', function () {
+    event.preventDefault();
+    const numProduct = this['num-product'].value;
+    const numRounds = this['num-rounds'].value;
+
+    const settings = {numProduct: numProduct, numRounds: numRounds};
+    localStorage.setItem('settings', JSON.stringify(settings));
+    
+});
